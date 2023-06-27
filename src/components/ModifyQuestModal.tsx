b9bec515c28487909a005916d226522cbd7fa5bb
Moon <adatx100@gmail.com>
2023-06-27 11:46:48 +0700

    demoo

--- src/components/ModifyQuestModal.tsx
+++ src/components/ModifyQuestModal.tsx
@@ -1,9 +1,9 @@
 import Modal from "../utils/modal";
-import {useAppContext} from "../providers/app-provider";
-import {useEffect, useState} from "react";
+import { useAppContext } from "../providers/app-provider";
+import { useEffect, useState } from "react";
 
 const ModifyQuestModal = () => {
-  const {questModal, setQuestModal, setQuestTypes} = useAppContext()
+  const { questModal, setQuestModal, setQuestTypes } = useAppContext()
 
   const defaultData = {
     name: '',
@@ -17,16 +17,16 @@ const ModifyQuestModal = () => {
   }, [questModal])
 
   const onClick = () => {
-    if (questModal.id != undefined) {
+    if (questModal.id !== undefined) {
       if (setQuestTypes) {
-        setQuestTypes(prep => {
+        setQuestTypes((prep: any) => {
           prep[questModal.id]= data
           return [...prep]
         })
       }
     } else {
       if (setQuestTypes) {
-        setQuestTypes(prep => [...prep, ...[data]])
+        setQuestTypes((prep: any) => [...prep, ...[data]])
       }
     }
     if (setQuestModal) {
@@ -35,46 +35,66 @@ const ModifyQuestModal = () => {
   }
 
   return (
-    <Modal title={'Add Quest'} isOpen={!!questModal}
-           onClose={() => setQuestModal && setQuestModal(undefined)}
-           body={(
-              <div className="p-5 flex flex-col gap-3">
-                <div className="flex gap-3 items-center">
-                  <span className="w-1/4">TypeID</span>
-                  <input className="border rounded flex-1 px-3 py-1" value={data.name} onInput={(e: any) => setData(prep => {
-                    return {
-                      ...prep,
-                      name: e.target?.value
-                    }
-                  })}/>
-                </div>
-                <div className="flex gap-3 items-center">
-                  <span className="w-1/4">TypeData</span>
-                  <select className="border rounded flex-1 px-3 py-1" value={data.dataType} onChange={(e: any) => setData(prep => {
-                    return {
-                      ...prep,
-                      dataType: e.target?.value
-                    }
-                  })}>
-                    <option value='string'>string</option>
-                    <option value='number'>number</option>
-                    <option value='boolean'>boolean</option>
-                  </select>
-                </div>
-                <div className="flex gap-3 items-center">
-                  <span className="w-1/4">Description</span>
-                  <input className="border rounded flex-1 px-3 py-1" value={data.description} onInput={(e: any) => setData(prep => {
-                    return {
-                      ...prep,
-                      description: e.target?.value
-                    }
-                  })}/>
-                </div>
-
-                <button onClick={onClick}>Save</button>
-              </div>
-            )}
+    <Modal
+      title={'Add Quest'}
+      isOpen={!!questModal}
+      onClose={() => setQuestModal && setQuestModal(undefined)}
+      body={
+        <div className="p-5 flex flex-col gap-3">
+          <div className="flex gap-3 items-center">
+            <span className="w-1/4">TypeID</span>
+            <input
+              className="border rounded flex-1 px-3 py-1"
+              value={data.name}
+              onInput={(e: any) =>
+                setData(prep => {
+                  return {
+                    ...prep,
+                    name: e.target?.value
+                  }
+                })
+              }
+            />
+          </div>
+          <div className="flex gap-3 items-center">
+            <span className="w-1/4">TypeData</span>
+            <select
+              className="border rounded flex-1 px-3 py-1"
+              value={data.dataType}
+              onChange={(e: any) =>
+                setData(prep => {
+                  return {
+                    ...prep,
+                    dataType: e.target?.value
+                  }
+                })
+              }
+            >
+              <option value="string">string</option>
+              <option value="number">number</option>
+              <option value="boolean">boolean</option>
+            </select>
+          </div>
+          <div className="flex gap-3 items-center">
+            <span className="w-1/4">Description</span>
+            <input
+              className="border rounded flex-1 px-3 py-1"
+              value={data.description}
+              onInput={(e: any) =>
+                setData(prep => {
+                  return {
+                    ...prep,
+                    description: e.target?.value
+                  }
+                })
+              }
+            />
+          </div>
+          <button onClick={onClick}>Save</button>
+        </div>
+      }
     />
   )
 }
+
 export default ModifyQuestModal;
